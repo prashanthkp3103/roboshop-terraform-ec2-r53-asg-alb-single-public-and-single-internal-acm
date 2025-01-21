5ad628c03e91ac6e2f00775bfbee5790cb791032
prasanthbangs2016 <prasanthbangs@gmail.com>
2025-01-21 17:45:59 +0530

    k8s

--- main.tf
+++ main.tf
@@ -15,44 +15,44 @@ module "vpc" {
 
 }
 
-module "apps" {
-  depends_on = [module.db, module.vpc]
-  source = "./modules/asg"
-
-  for_each      = var.apps  #this comes from dev main.tfvars
-  #In the below code each.key is catalogue(for name)
- # apps = {
- #   catalogue = {
- #     instance_type = ""
-  #  }
-  #}
-  #it is coming from main.tfvars in env-dev
-  name          = each.key  #this comes from dev main.tfvars apps
-  instance_type = each.value["instance_type"]
-  allow_port    = each.value["allow_port"]
-  allow_sg_cidr = each.value["allow_sg_cidr"]
-  #below value comes from vpc module outputs
-  #subnet      = module.vpc.subnets["web"][0]
-  subnet_ids    = module.vpc.subnets[each.value["subnet_ref"]]
-  capacity = each.value["capacity"]
-  #below value comes from vpc module outputs
-  vpc_id        = module.vpc.vpc_id
-  env           = var.env
-  bastion_node  = var.bastion_node
-  asg           = true
-  vault_token   = var.vault_token
-  zone_id = var.zone_id
-  #below property for lb
-  #internal      = each.value["lb_internal"]
-  #lb_subnet_ids = module.vpc.subnets[each.value["lb_subnet_ref"]]
-  #allow_lb_sg_cidr = each.value["allow_lb_sg_cidr"]
-  #acm_http_arn      = each.value["acm_http_arn"]
-  # it will get public dns and private dns names
-  dns_name          = module.load-balancers[each.value["lb_ref"]].dns_name
-  listener_arn = module.load-balancers[each.value["lb_ref"]].listener_arn
-  lb_rule_priority = each.value["lb_rule_priority"]
-
-}
+# module "apps" {
+#   depends_on = [module.db, module.vpc]
+#   source = "./modules/asg"
+#
+#   for_each      = var.apps  #this comes from dev main.tfvars
+#   #In the below code each.key is catalogue(for name)
+#  # apps = {
+#  #   catalogue = {
+#  #     instance_type = ""
+#   #  }
+#   #}
+#   #it is coming from main.tfvars in env-dev
+#   name          = each.key  #this comes from dev main.tfvars apps
+#   instance_type = each.value["instance_type"]
+#   allow_port    = each.value["allow_port"]
+#   allow_sg_cidr = each.value["allow_sg_cidr"]
+#   #below value comes from vpc module outputs
+#   #subnet      = module.vpc.subnets["web"][0]
+#   subnet_ids    = module.vpc.subnets[each.value["subnet_ref"]]
+#   capacity = each.value["capacity"]
+#   #below value comes from vpc module outputs
+#   vpc_id        = module.vpc.vpc_id
+#   env           = var.env
+#   bastion_node  = var.bastion_node
+#   asg           = true
+#   vault_token   = var.vault_token
+#   zone_id = var.zone_id
+#   #below property for lb
+#   #internal      = each.value["lb_internal"]
+#   #lb_subnet_ids = module.vpc.subnets[each.value["lb_subnet_ref"]]
+#   #allow_lb_sg_cidr = each.value["allow_lb_sg_cidr"]
+#   #acm_http_arn      = each.value["acm_http_arn"]
+#   # it will get public dns and private dns names
+#   dns_name          = module.load-balancers[each.value["lb_ref"]].dns_name
+#   listener_arn = module.load-balancers[each.value["lb_ref"]].listener_arn
+#   lb_rule_priority = each.value["lb_rule_priority"]
+#
+# }
 
 #refering to vpc module output
 output "web" {
