c64939a4c54a26cdb404df6c8909806cd0a45861
prasanthbangs2016 <prasanthbangs@gmail.com>
2025-01-21 18:46:46 +0530

    k8s

--- modules/asg/main.tf
+++ modules/asg/main.tf
@@ -50,6 +50,13 @@ resource "aws_launch_template" "main" {
     market_type = "spot"
   }
 
+  block_device_mappings {
+    device_name = "/dev/sda1"
+    ebs {
+      volume_size = 35
+    }
+  }
+
   user_data   = base64encode(templatefile("${path.module}/userdata.sh", {
     env       = var.env
     role_name       = var.name
