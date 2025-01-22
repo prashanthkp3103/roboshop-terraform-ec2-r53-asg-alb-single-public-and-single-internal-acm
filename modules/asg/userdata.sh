cbc926e28729dc0b5088f67a2809745eabb4818b
prasanthbangs2016 <prasanthbangs@gmail.com>
2025-01-22 13:59:14 +0530

    docker

--- modules/asg/userdata.sh
+++ modules/asg/userdata.sh
@@ -8,3 +8,4 @@ lvextend -r -L +10G /dev/mapper/RootVG-varVol
 pip3.11 install ansible hvac 2>&1 | tee -a /opt/userdata.log
 ansible-pull -i localhost, -U https://github.com/prashanthkp3103/roboshop-latest-ansible main.yml -e env=${env} -e role_name=docker -e app_name=${app_name} -e vault_token=${vault_token} 2>&1 | tee -a /opt/userdata.log
 ##vault token comes from github secrets#
+#log file /opt/userdata.log
